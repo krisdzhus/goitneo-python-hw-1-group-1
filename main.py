a56b85af4ef82beb3946978ce54485718e7d5a6a
Kris <dzhuskristina@gmail.com>
2024-02-21 15:50:37 +0100

    add "assistant"

--- main.py
+++ main.py
@@ -1,31 +1,29 @@
 from datetime import datetime, timedelta
 
 
-def get_birthdays_per_week(users):
+def get_birthdays_per_week(users: list[dict]):
 
 # Визначаємо поточну дату
-  today = datetime.now().date()
+    today = datetime.now().date()
 # Визначаємо дату понеділка на поточному тижні
-  monday = (today - timedelta(days=today.weekday()))
-
+    monday = (today - timedelta(days=today.weekday()))
 # Визначаємо дату понеділка на наступному тижні
-  next_monday = monday + timedelta(weeks=1)
-
+    next_monday = monday + timedelta(weeks=1)
 # Визначаємо дату останнього дня на наступному тижні
-  end_of_next_week = next_monday + timedelta(days=7)
+    end_of_next_week = next_monday + timedelta(days=7)
 
-  #Аналіз дати народження
-  birthday_dict = {}
-  for user in users:  
+#Аналіз дати народження
+    birthday_dict = {}
+    for user in users:  
         #Отримуємо дату народження
         birthday = user["birthday"].date()
         birthday_this_year = birthday.replace(year=today.year)
-  # Перевіряємо, чи вже минув день народження цього року
+# Перевіряємо, чи вже минув день народження цього року
         if birthday_this_year < today:
-  # Якщо так, то розглядаємо дату на наступний рік
+# Якщо так, то розглядаємо дату на наступний рік
             birthday_this_year = birthday.replace(year=today.year+1)
 
-    # Перевіряємо чи ця дата наступного тижня
+# Перевіряємо чи ця дата наступного тижня
         if next_monday <= birthday_this_year < end_of_next_week:
             weekday = birthday_this_year.strftime('%A')
             if weekday == "Saturday" or weekday == "Sunday":
@@ -36,7 +34,7 @@ def get_birthdays_per_week(users):
             
             birthday_dict[weekday].append(user['name'])
 
-  print(birthday_dict)
+    print(birthday_dict)
 
 
 # Тестуємо
@@ -48,7 +46,3 @@ users = [
 ]
 
 get_birthdays_per_week(users)
-
-
-if __name__ == "__main__":
-    main()
